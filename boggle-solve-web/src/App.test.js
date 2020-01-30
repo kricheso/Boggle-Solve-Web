6b5f8996a416f28d4b206042f294987fb5835d6f
Kousei Richeson <kousei@nmsu.edu>
2020-01-29 21:08:02 -0800

    Added Unit Tests

--- boggle-solve-web/src/App.test.js
+++ boggle-solve-web/src/App.test.js
@@ -2,8 +2,33 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders mobile app link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Mobile app here!/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Make sure the url goes to my apple website', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Mobile app here!/i);
+  expect(linkElement.href).toEqual("https://apps.apple.com/us/app/boggle-solve/id1496483167");
+});
+
+test("Make sure the button does not say end game when first loaded", () => {
+const { getByText } = render(<App />);
+let endButton = null;
+try { endButton = getByText(/End Game/i); } catch { endButton = null }
+expect(endButton).not.toBeTruthy();
+});
+
+test('renders title on the page', () => {
+  const { getByText } = render(<App />);
+  const title = getByText(/Boggle Solve Web/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('renders button on the page', () => {
+  const { getByText } = render(<App />);
+  const button = getByText(/New Game/i);
+  expect(button).toBeInTheDocument();
 });
